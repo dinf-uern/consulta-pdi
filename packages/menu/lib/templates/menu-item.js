37c15fadb94739c5b79851eb6daaa39ad3aba1da
Luis <luisinweb@gmail.com>
2015-09-23 08:52:14 -0300

    estrutura do pacote de menus

---
+++ packages/menu/lib/templates/menu-item.js
@@ -0,0 +1,37 @@
+Template.menuItem.helpers({
+    dataItem: function(){
+        return this.dataItem.get();
+    },
+    getSubmenusStates: function(){
+        var statesArr = [];
+        if(this.subitems.get() && this.subitems.get().length > 0)
+            statesArr = _.map(this.subitems.get(), function(item){
+                return item.dataItem.get().state;
+            });
+
+        return statesArr;
+    },
+    submenus: function(){
+        var user = Meteor.user();
+        var roles = user && user.roles ? user.roles: [];
+
+        return this.getSubitems(user, roles);
+    },
+    link: function(){
+        var result = "#";
+        if (this.dataItem.get() && this.dataItem.get().link)
+            result = this.dataItem.get().link;
+
+        if(this.dataItem.get() && this.dataItem.get().state)
+            result = Router.routes[this.dataItem.get().state].path();
+
+        return result;
+    },
+    target: function(){
+        result = "";
+        if (this.dataItem.get().target)
+            result = this.dataItem.get().target;
+
+        return result;
+    }
+});
