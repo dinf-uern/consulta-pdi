f15920afbd9d27ff6eb7f61b700fa9bacb836edf
Luis <luisinweb@gmail.com>
2015-10-06 08:15:06 -0300

    modificado para indicar existencia de propostas de inclusao homologadas

--- pdi/client/propostas/componentes/status/inclusion-count-icon.js
+++ pdi/client/propostas/componentes/status/inclusion-count-icon.js
@@ -1,3 +1,9 @@
+Template.inclusionCountIcon.helpers({
+    classBadgeSuccess: function(){
+        return this.node.temPropostaInclusaoHomologada === true? 'badge-success':'';
+    }
+});
+
 Template.inclusionCountIcon.events({
     'click .indicador': function(event, template){
         var loggedInUser = Meteor.user();
