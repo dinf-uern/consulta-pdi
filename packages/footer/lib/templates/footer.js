47ddec493fa70c8ee26120a418378ad44eca870d
Luis <luisinweb@gmail.com>
2015-10-07 14:47:44 -0300

    adicionado numero da versão ao rodapé

--- packages/footer/lib/templates/footer.js
+++ packages/footer/lib/templates/footer.js
@@ -2,5 +2,8 @@ Template.footer.helpers({
     ano: function(){
         var dateOnServer = Session.get('dateOnServer');
         return moment(dateOnServer).format('YYYY');
+    },
+    version: function(){
+        return Footer.getVersion();
     }
 });
